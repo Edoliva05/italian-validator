27362825bf369c55109e1d36bc0839f6e8e74cdc
Edoardo Oliva <edoliva05@gmail.com>
2026-07-21 11:43:05 +0200

    Crata funzione di validazione CF con regex e happy path, e test sulla lunghezza

--- src/main/java/com/edoardo/validator/ItalianValidator.java
+++ src/main/java/com/edoardo/validator/ItalianValidator.java
@@ -1,5 +1,19 @@
-package main.java.com.edoardo.validator;
+package com.edoardo.validator;
 
 public class ItalianValidator {
     
+    public static boolean isValidCodiceFiscale(String codiceFiscale){
+
+        //Se è null
+        if(codiceFiscale == null) return false;
+        
+        String cf = codiceFiscale.trim().toUpperCase();
+
+        //6 lettere, 2 numeri, 1 lettera, 2 numeri, 1 lettera, 3 numeri, 1 lettera
+        String regex = "^[A-Z]{6}\\d{2}[A-Z]\\d{2}[A-Z]\\d{3}[A-Z]$";
+
+        //restituisce true se ripetta il pattern
+        return cf.matches(regex);
+
+    }
 }
--- src/test/java/com/edoardo/validator/ItalianValidatorTest.java
+++ src/test/java/com/edoardo/validator/ItalianValidatorTest.java
@@ -1,5 +1,23 @@
-package test.java.com.edoardo.validator;
+package com.edoardo.validator;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
 public class ItalianValidatorTest {
     
+    @Test
+    void shouldReturnTrueWhenCodiceFiscaleIsPerfectlyValid(){
+        String cf = "RSSMRA80A01H501U";
+        boolean result = ItalianValidator.isValidCodiceFiscale(cf);
+        assertTrue(result, "Il metodo dovrebbe restituire true per un CF corretto");
+    }
+
+    @Test
+    void shouldReturnFalseWhenCodiceFiscaleIsTooShort(){
+        String short_cf = "RSSMRA80A01H5";
+        boolean result = ItalianValidator.isValidCodiceFiscale(short_cf);
+        assertFalse(result, "Il metodo dovrebbe restituire false per un CF troppo corto");
+    }
+
 }
