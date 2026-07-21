759203a5ea1ea4ced42ac7671a620bd8cee2125f
Edoardo Oliva <edoliva05@gmail.com>
2026-07-21 17:36:28 +0200

    Aggiunti i tag in Controller

--- src/main/java/com/edoardo/validator/CodiceFiscaleController.java
+++ src/main/java/com/edoardo/validator/CodiceFiscaleController.java
@@ -6,14 +6,24 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
+@Tag(name = "Validazione Fiscale", description = "Endpoint per il controllo e l'estrazione dati dal Codice Fiscale Italiano")
 public class CodiceFiscaleController {
 
     @GetMapping("/api/valida/{cf}")
-    public Map<String, Object> validaCF(@PathVariable String cf) {
-        
-        // Map che SpringBoot trasformerà in un JSON
+    @Operation(
+        summary = "Valida ed estrae i dati anagrafici", 
+        description = "Passando un codice fiscale valido di 16 caratteri alfanumerici, restituisce data di nascita, sesso e codice catastale del comune."
+    )
+    public Map<String, Object> validaCF(
+
+            @Parameter(description = "Il Codice Fiscale da validare (es. RSSMRA80A01H501U)", required = true) 
+            @PathVariable String cf) {
+            
         Map<String, Object> response = new HashMap<>();
         
         boolean isValid = ItalianValidator.isValidCodiceFiscale(cf);
@@ -21,14 +31,12 @@ public class CodiceFiscaleController {
         response.put("codiceFiscale", cf.toUpperCase());
         response.put("valido", isValid);
 
-        // Se è valido, aggiungiamo tutti i dati extra
         if (isValid) {
             response.put("dataNascita", ItalianValidator.extractBirthDate(cf));
             response.put("sesso", ItalianValidator.extractGender(cf));
             response.put("comune", ItalianValidator.extractCodiceCatastale(cf));
         } else {
-            // Se non è valido, mandiamo un messaggio di errore 
-            response.put("errore", "Il codice fiscale fornito non è valido o contiene errori");
+            response.put("errore", "Il codice fiscale fornito non è valido o contiene errori.");
         }
 
         return response;
